6c75a7e9020271fa1622572add7ad39182795fe6
ArsacMohammed <arsacskasha@gmail.com>
2023-06-08 18:37:49 +0530

    Working with 2d  arrays

--- basics_first_program/src/basics_first_program/my_sweetprogram.java
+++ basics_first_program/src/basics_first_program/my_sweetprogram.java
@@ -1,37 +1,19 @@
 package basics_first_program;
 
-import java.util.Scanner;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.io.File;
-import java.util.ListIterator;
-import java.io.FileNotFoundException; 
-
-
 public class my_sweetprogram {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LinkedList<String> names =new LinkedList<String>();
-		names.push("caelb");
-		names.push("kate");
-		names.push("nice");
-    	
-		ListIterator <String> it =names.listIterator();
-		it.next();
-		it.next();
-		it.add("susan");
+public static void main(String[] args) {
+	int [][] data ={
+		{1,2,3,4},
+		{1,2,5,7},
+		{2,3,8,6}
 
-		
-//		while (it.hasNext()) {
-//			System.out.println(it.next());
-//		}
-		for (String s : names) {
-			System.out.println(s);
+	};
+	for (int i=0;i<data.length;i++) {
+		for (int j=0;j<data[i].length;j++) {
+			System.out.print(data[i][j]+" ");
+			}
+		System.out.println();
 		}
-
-//		System.out.println(it.next());
+	
 	}
-
 }
