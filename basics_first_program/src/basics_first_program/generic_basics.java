f6f8c315ec15f82f019507bbeeae851e6e2dc05a
ArsacMohammed <arsacskasha@gmail.com>
2023-06-11 18:09:49 +0530

    wildcard to extend to super class

--- basics_first_program/src/basics_first_program/generic_basics.java
+++ basics_first_program/src/basics_first_program/generic_basics.java
@@ -3,34 +3,18 @@ import java.util.ArrayList;
 public class generic_basics {
 
 	public static void main(String[] args) {
-//		Pair<String,Integer> pair =new Pair<String,Integer>();
-//		pair.setX("caleb");
-//		pair.setY(10);
-//		Pair<String,Integer> pair1 =new Pair<String,Integer>();
-//		pair1.setX("caleb");
-//		pair1.setY(10);
-//		
-//		ArrayList<Pair<String,Integer>> pears = new ArrayList<Pair<String,Integer>>();
-//		pears.add(pair1);
-//		pears.add(pair);
-		
-		ArrayList<Person> people =new ArrayList<Person>();
+		ArrayList<Person> people = new ArrayList<Person>();
 		Admin a =new Admin();
 		people.add(a);
-		
 		ArrayList<Admin> admins = new ArrayList<Admin>();
-
 		doSomething(admins);
 	}
 
-	private static void doSomething(ArrayList<Admin> a) {
-		System.out.println(a);
+	private static void doSomething(ArrayList<? extends Person> peeps) {
+		//? extends Person means any arraylist type that extends Person class is acceptable.
+		System.out.println(peeps);
 		
 	}
 
-//	private static void doSomething(Person p) {
-//		System.out.println(p);
-//		
-//	}
-
 }
+/* if a manage class is inherited from the employee class then they have a relationship but list of employee and list of manager has no relationship at all */
