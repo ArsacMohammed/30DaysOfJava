075d7b114332dc22eb0d4d299c61e19bb19a4253
ArsacMohammed <arsacskasha@gmail.com>
2023-06-11 13:08:58 +0530

    hashset

--- basics_first_program/src/basics_first_program/OverideHashcodeAndEqual.java
+++ basics_first_program/src/basics_first_program/OverideHashcodeAndEqual.java
@@ -1,23 +1,39 @@
 package basics_first_program;
+import java.util.HashSet;
+import java.util.HashMap;
 
 public class OverideHashcodeAndEqual {
 	public static void main(String[] args) {
-	Person p =new Person("arsac@gmail.com","caleb");
-	Position pos1=new Position(10,10,20);
-	p.position =pos1;
-	Person q =new Person("arsac@gmail.com","caleb");
-	Position pos2 =new Position (10,10,20);
-	q.position=pos2;
-	System.out.println(p.hashCode());
-	System.out.println(q.hashCode());
-//	System.out.println(p.equals(q));
-//	System.out.println(p.hashCode());
-//	System.out.println(q.hashCode());
-//	
-//	Object q1="jelly";
-//	Object p1="jelly";
-//	System.out.println(p.equals(q));
-//	System.out.println(p1.hashCode());
-//	System.out.println(q1.hashCode());
+		HashSet<Person> people =new HashSet<Person>();
+		Person p =new Person("arsac@gmail.com","caleb");
+		Position pos1=new Position(10,10,20);
+		p.position =pos1;
+		Person q =new Person("arsac@gmail.com","caleb");
+		Position pos2 =new Position (10,10,20);
+		q.position=pos2;
+		people.add(p);
+		people.add(q);
+		
+		HashMap<String,Person> peeps =new HashMap<String,Person>();
+		peeps.put("arsac@gmail.com",p);
+		peeps.put("arsac@gmail.com",q);
+		System.out.println("hashmap: "+(peeps.containsKey("arsac@gmail.com")));
+		
+		
+		
+		
+		
+//		names.add("rusthum");
+//		names.add("kite");
+//		names.add("mickel");
+//		names.add("adam");
+//		names.add("abdul");
+//		for (String name:names) {
+//			System.out.println(name);
+//		}
+//		
+;
+ 
+ */
 	}
 }
