ee165e3c601fcd4fe53adefbc4369e43888ae973
ArsacMohammed <arsacskasha@gmail.com>
2023-06-12 22:31:17 +0530

    getting book details and storing inside the arraylist

--- LibrarayManagementSystem/src/LibrarayManagementSystem/Book.java
+++ LibrarayManagementSystem/src/LibrarayManagementSystem/Book.java
@@ -1,14 +1,20 @@
 package LibrarayManagementSystem;
-
+import java.util.*;
 public class Book {
 	String title;
-	String author;
+	String authorName;
 	long ISBN ;
-	int Quantity;
-	
-	public void  addBook() {
-		
+	int quantity;
+	public Book(String title,String authorName,long ISBN ,int quantity) {
+		this.title=title;
+		this.authorName=authorName;
+		this.ISBN=ISBN;
+		this.quantity=quantity;
 	}
+	public String  getTitle() {
+		return title;
+	}
+	
 	public void  removeBook() {
 		
 	}
--- LibrarayManagementSystem/src/LibrarayManagementSystem/LibrarayManagementSystem.java
+++ LibrarayManagementSystem/src/LibrarayManagementSystem/LibrarayManagementSystem.java
@@ -1,7 +1,26 @@
 package LibrarayManagementSystem;
-/**/
+import java.util.ArrayList;
+import java.util.*;
 public class LibrarayManagementSystem {
+//  getting input for a book and storing it inside an arraylist.
+	static Scanner in =new Scanner(System.in);
+	static ArrayList<Book> books =new ArrayList<Book>();
+	
 	public static void main(String[] args) {
+
+		System.out.println("enter the add option  as 1");
+		int  addOption= in.nextInt();
+		if (addOption == 1) {
+			doAddBook();
+		}
+		System.out.println(books.size());
+	}
+
+	private static void doAddBook() {
+		// TODO Auto-generated method stub
+		System.out.println("enter the book details in order as title,authorname,ISBN,Quantity");
+		Book book1 =new Book (in.next(),in.next(),in.nextInt() ,in.nextInt());
+		books.add(book1);
 		
 	}
 }
