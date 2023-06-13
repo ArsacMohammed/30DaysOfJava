21f579872390d48f573407dc37f081a11669ab63
ArsacMohammed <arsacskasha@gmail.com>
2023-06-13 08:41:28 +0530

    adding and removing the book is completed

--- LibrarayManagementSystem/src/LibrarayManagementSystem/Book.java
+++ LibrarayManagementSystem/src/LibrarayManagementSystem/Book.java
@@ -14,12 +14,37 @@ public class Book {
 	public String  getTitle() {
 		return title;
 	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(title,authorName,ISBN,quantity);
 	
-	public void  removeBook() {
-		
 	}
-	public void updateBook() {
-		
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (ISBN != other.ISBN)
+			return false;
+		if (authorName == null) {
+			if (other.authorName != null)
+				return false;
+		} else if (!authorName.equals(other.authorName))
+			return false;
+		if (quantity != other.quantity)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
 	}
+	
+	
 
 }
--- LibrarayManagementSystem/src/LibrarayManagementSystem/LibrarayManagementSystem.java
+++ LibrarayManagementSystem/src/LibrarayManagementSystem/LibrarayManagementSystem.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.*;
 public class LibrarayManagementSystem {
 //  getting input for a book and storing it inside an arraylist.
+	
 	static Scanner in =new Scanner(System.in);
 	static ArrayList<Book> books =new ArrayList<Book>();
 	
@@ -14,13 +15,45 @@ public class LibrarayManagementSystem {
 			doAddBook();
 		}
 		System.out.println(books.size());
+		System.out.println("do you want to remove the book");
+		int removeOption =in.nextInt();
+		if (removeOption==1) {
+			doRemoveBook();
+		}
+		
+		System.out.println(books.size());
+	
 	}
 
 	private static void doAddBook() {
-		// TODO Auto-generated method stub
-		System.out.println("enter the book details in order as title,authorname,ISBN,Quantity");
-		Book book1 =new Book (in.next(),in.next(),in.nextInt() ,in.nextInt());
-		books.add(book1);
-		
+	    System.out.println("Enter the book details in order as title, author name, ISBN, Quantity:");
+	    String title = in.next();
+	    String authorName = in.next();
+	    long ISBN = in.nextLong();
+	    int quantity = in.nextInt();
+	    Book book1 = new Book(title, authorName, ISBN, quantity);
+		books.add(book1);		
 	}
+	
+	private static void doRemoveBook() {
+	    System.out.println("Enter the book details in order as title, author name, ISBN, Quantity:");
+	    String title = in.next();
+	    String authorName = in.next();
+	    long ISBN = in.nextLong();
+	    int quantity = in.nextInt();
+	    
+	    // Create a temporary book object with the given details for searching
+	    Book bookToRemove = new Book(title, authorName, ISBN, quantity);
+	    
+	    if (books.contains(bookToRemove)) {
+	        books.remove(bookToRemove);
+	        System.out.println("Book removed successfully.");
+	    } else {
+	        System.out.println("Book not found.");
+	    }
+	}
+
+		
+		
+		
 }
