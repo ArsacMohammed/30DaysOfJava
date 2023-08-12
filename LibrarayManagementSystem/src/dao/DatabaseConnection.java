d9c84b33f2fcfc171205fd2481848b2f6ac358eb
ArsacMohammed <arsacskasha@gmail.com>
2023-08-12 17:14:42 +0530

    crated database class in dao for postgres connection and added postgres jdbc to classpath of the library tab in buildpath of the library_management_system

--- LibrarayManagementSystem/src/dao/DatabaseConnection.java
+++ LibrarayManagementSystem/src/dao/DatabaseConnection.java
@@ -1,10 +1,30 @@
 package dao;
-
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 public class DatabaseConnection {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private static final String DB_URL ="jdbc:postgresql://localhost:5432/library_management_system";
+	private static final String USER ="postgres";
+	private static final String PASS="Arsac@07";
+			
+	
+	public static Connection getConnection() throws SQLException{
+		return DriverManager.getConnection(DB_URL,USER,PASS);
 	}
+	
+	 public static void main(String[] args) {
+	        // You can use the getConnection method here
+	        try {
+	            Connection connection = getConnection();
+	            System.out.println("Connection successful");
+	            // Perform any database operations as needed
+	            // Remember to close the connection when done
+	            connection.close();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
 
 }
