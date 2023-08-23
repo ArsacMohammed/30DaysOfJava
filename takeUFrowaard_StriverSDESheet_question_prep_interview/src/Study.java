2d15814b9d8e566b12c7dbe6a05968b9a2f42dad
ArsacMohammed <arsacskasha@gmail.com>
2023-08-23 17:06:18 +0530

    removed unwanted char

---
+++ takeUFrowaard_StriverSDESheet_question_prep_interview/src/Study.java
@@ -0,0 +1,10 @@
+
+public class Study {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(100+100+"hello");
+		System.out.println("helo"+100+100);
+	}
+
+}
